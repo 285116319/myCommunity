0795f62a30987632b2f46595c9b69074b7595d53
wyban <285116319@qq.com>
2019-12-26 13:32:00 +0800

    add index and bootstrap

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # myCommunity
+
+##资料
+https://spring.io/
+https://spring.io/guides/gs/serving-web-content/
+https://v3.bootcss.com/components/
--- src/main/java/com/wyban/community/HelloController/IndexController.java
+++ src/main/java/com/wyban/community/HelloController/IndexController.java
@@ -9,12 +9,11 @@ import org.springframework.web.bind.annotation.RequestParam;
  * Create by wyban on 2019/12/24
  */
 @Controller
-public class HelloController {
+public class IndexController {
 
-    @GetMapping("/hello")
-    public String hello(@RequestParam(name = "name")String name, Model model){
-        model.addAttribute("name",name);
-        return "hello";
+    @GetMapping("/")
+    public String index(){
+        return "index";
 
     }
 }
